2a2dfeb4e3758234c0184eccf56d786d0bdaa0cc
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-02-20 22:54:39 +0900

    [ADD] BaseEntity 추가. - createdAt, updatedAt을 가진 BaseEntity를 이용하여 모든 엔티티에 생성, 수정 시간 추가.

---
+++ src/main/java/com/example/demo/model/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(columnDefinition = "TIMESTAMP")
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(columnDefinition = "TIMESTAMP")
+	private LocalDateTime updatedAt;
+}
