9c29caeca9a56701854ebfdf39a4b88572b15526
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-02-21 03:16:32 +0900

    [FIX] Battle 엔티티 수정. - AttributeOverride를 이용한 컬럼명 오버라이드. - AssociationOverride를 이용한 fk 오버라이드.

--- src/main/java/com/example/demo/model/battle/Battle.java
+++ src/main/java/com/example/demo/model/battle/Battle.java
@@ -1,10 +1,14 @@
 package com.example.demo.model.battle;
 
+import javax.persistence.AssociationOverride;
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 
 import com.example.demo.model.BaseEntity;
 
@@ -21,8 +25,12 @@ public class Battle extends BaseEntity {
 	private Long id;
 
 	@Embedded
+	@AttributeOverride(name = "voteCount", column = @Column(name = "challenged_vote_count"))
+	@AssociationOverride(name = "post", joinColumns = @JoinColumn(name = "challenged_post_id"))
 	private BattleInfo challengedPost;
 
 	@Embedded
+	@AttributeOverride(name = "voteCount", column = @Column(name = "challenging_vote_count"))
+	@AssociationOverride(name = "post", joinColumns = @JoinColumn(name = "challenging_post_id"))
 	private BattleInfo challengingPost;
 }
