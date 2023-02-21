c2caf315457646d381f6b0d2b5dd38aeaa09cc4a
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-02-21 21:15:05 +0900

    [TEST] PostValidationTest 추가. - TestConstant로 재사용할 수 있을 것 같은 200자를 넘는 문자열 빼내기. - Post에서 application level에서 검증하는 로직 추가. - Music에 대한 application level에서의 검증 로직 추가. - Like는 validation할 부분이 존재하지 않아서 테스트 코드 추가하지 않음.

---
+++ src/test/java/com/example/demo/model/post/PostValidationTest.java
@@ -0,0 +1,136 @@
+package com.example.demo.model.post;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PostValidationTest {
+
+	private static final String musicId = "A123EB";
+	private static final String albumCoverUrl = "https://localhost:8080/album/1";
+	private static final String singer = "뉴진스";
+	private static final String title = "Ditto";
+	private static final Genre genre = Genre.DANCE;
+	private static final String musicUrl = "https://localhost:8080/music/1";
+	private static final String content = "Hype(하입) 파이팅!!";
+	private static final int likeCount = 10;
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@ValueSource(strings = {
+		"             ",
+		"내용이 있는 경우"
+	})
+	public void 성공_Post를_생성할_수_있다(String value) {
+		// TODO : 더 명확히 Post 생성 성공을 나타내는 테스트 방법이 있을지
+		var post = new Post(musicId, albumCoverUrl, singer, title, genre, musicUrl, value, likeCount);
+		assertThat(post).isExactlyInstanceOf(Post.class);
+	}
+
+	@Test
+	public void 실패_좋아요_개수가_음수면_Post를_생성할_수_없다() {
+		assertThatThrownBy(() -> {
+			new Post(musicId, albumCoverUrl, singer, title, genre, musicUrl, content, -1);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@ValueSource(strings = {
+		" ",
+		"     "
+	})
+	public void 실패_음악_고유_번호가_null이거나_공백이면_Post를_생성할_수_없다(String value) {
+		assertThatThrownBy(() -> {
+			new Post(value, albumCoverUrl, singer, title, genre, musicUrl, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@ValueSource(strings = {
+		" ",
+		"     "
+	})
+	public void 실패_앨범_커버_URL이_null이거나_공백이면_Post를_생성할_수_없다(String value) {
+		assertThatThrownBy(() -> {
+			new Post(musicId, value, singer, title, genre, musicUrl, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void 실패_앨범_커버_URL_길이가_2000자가_넘으면_Post를_생성할_수_없다() {
+		assertThatThrownBy(() -> {
+			new Post(musicId, TestConstant.OVER_LENGTH_2000, singer, title, genre, musicUrl, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@ValueSource(strings = {
+		" ",
+		"     "
+	})
+	public void 실패_가수_이름이_null이거나_공백이면_Post를_생성할_수_없다(String value) {
+		assertThatThrownBy(() -> {
+			new Post(musicId, albumCoverUrl, value, title, genre, musicUrl, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@ValueSource(strings = {
+		" ",
+		"     "
+	})
+	public void 실패_음악_제목이_null이거나_공백이면_Post를_생성할_수_없다(String value) {
+		assertThatThrownBy(() -> {
+			new Post(musicId, albumCoverUrl, singer, value, genre, musicUrl, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void 실패_음악_장르가_null이면_Post를_생성할_수_없다() {
+		assertThatThrownBy(() -> {
+			new Post(musicId, albumCoverUrl, singer, title, null, musicUrl, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@ValueSource(strings = {
+		" ",
+		"     "
+	})
+	public void 실패_음악_URL이_null이거나_공백이면_Post를_생성할_수_없다(String value) {
+		assertThatThrownBy(() -> {
+			new Post(musicId, albumCoverUrl, singer, title, genre, value, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void 실패_음악_URL_길이가_2000자가_넘으면_Post를_생성할_수_없다() {
+		assertThatThrownBy(() -> {
+			new Post(musicId, albumCoverUrl, singer, title, genre, TestConstant.OVER_LENGTH_2000, content, likeCount);
+		})
+			.isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+}
