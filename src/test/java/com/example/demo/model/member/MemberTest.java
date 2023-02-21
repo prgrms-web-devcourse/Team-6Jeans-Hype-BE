a02c935a080c6a4c09f1c8383c479f2f9b96a23a
thguss <thgus345@naver.com>
2023-02-21 22:31:53 +0900

    [FIX] null & empty 테스트 묶음 및 테스트명 변경

--- src/test/java/com/example/demo/model/member/MemberTest.java
+++ src/test/java/com/example/demo/model/member/MemberTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -11,202 +14,125 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 class MemberTest {
 
+	private final int ranking = 1;
+	private final int victoryPoint = 1000;
+	private final int victoryCount = 10;
+	private final String profileImageUrl = "profileImage";
+	private final String nickname = "hype";
+	private final String refreshToken = "token";
+	private final Social socialType = Social.GOOGLE;
+	private final String socialId = "socialId";
+
 	@Test
-	void 실패_랭킹은_음수일수_없다() {
-		// given
+	void 성공_Member_객체를_생성할_수_있다() {
+		assertThat(createMember(profileImageUrl, nickname, refreshToken, socialType, socialId)).isExactlyInstanceOf(
+			Member.class);
+	}
+
+	@Test
+	void 실패_랭킹은_음수일_수_없다() {
+		MemberScore memberScore = new MemberScore();
 		int minusRanking = -1;
 
-		MemberScore memberScore = new MemberScore();
-
-		// when then
-		assertThatThrownBy(() -> memberScore.update(minusRanking, 1000, 10))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("랭킹이 음수일 수 없습니다. [" + minusRanking + "]");
+		assertThatThrownBy(() -> memberScore.update(minusRanking, victoryPoint, victoryCount)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
 	}
 
 	@Test
-	void 실패_승리포인트는_음수일수_없다() {
-		// given
+	void 실패_승리_포인트는_음수일_수_없다() {
+		MemberScore memberScore = new MemberScore();
 		int minusPoint = -1000;
 
-		MemberScore memberScore = new MemberScore();
-
-		// when then
-		assertThatThrownBy(() -> memberScore.update(1, minusPoint, 10))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("승리 포인트가 음수일 수 없습니다. [" + minusPoint + "]");
+		assertThatThrownBy(() -> memberScore.update(ranking, minusPoint, victoryCount)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
 	}
 
 	@Test
-	void 실패_승리횟수는_음수일수_없다() {
-		// given
+	void 실패_승리_횟수는_음수일_수_없다() {
+		MemberScore memberScore = new MemberScore();
 		int minusCount = -10;
 
-		MemberScore memberScore = new MemberScore();
-
-		// when then
-		assertThatThrownBy(() -> memberScore.update(1, 1000, minusCount))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("승리 횟수가 음수일 수 없습니다. [" + minusCount + "]");
+		assertThatThrownBy(() -> memberScore.update(ranking, victoryPoint, minusCount)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
 	}
 
 	@Test
-	void 성공_Member_에서_MemberScore_를_업데이트할수있다() {
-		// given
-		int ranking = 1;
-		int victoryPoint = 1000;
-		int victoryCount = 10;
+	void 성공_Member_에서_MemberScore_를_업데이트_할_수_있다() {
+		Member member = createMember(profileImageUrl, nickname, refreshToken, socialType, socialId);
 
-		// when
-		Member member = createMember("url", "닉네임", ranking,
-			victoryPoint, victoryCount, "token", Social.GOOGLE, "1234");
-
-		// then
 		assertThat(member.getMemberScore().getRanking()).isEqualTo(ranking);
 		assertThat(member.getMemberScore().getVictoryPoint()).isEqualTo(victoryPoint);
 		assertThat(member.getMemberScore().getVictoryCount()).isEqualTo(victoryCount);
 	}
 
-	@Test
-	void 실패_socialType_은_null_일수없다() {
-		// given
-		Social socialType = null;
+	@ParameterizedTest
+	@NullSource
+	void 실패_socialType_은_null_일_수_없다(Social value) {
+		assertThatThrownBy(
+			() -> createMember(profileImageUrl, nickname, refreshToken, value, socialId)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
+	}
 
-		// when then
-		assertThatThrownBy(() -> createMember("url", "닉네임",
-			1, 1000, 10, "token", socialType, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("소셜 타입은 Null 일 수 없습니다. [" + socialType + "]");
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	void 실패_socialId_는_null_이거나_공백일_수_없다(String value) {
+		assertThatThrownBy(
+			() -> createMember(profileImageUrl, nickname, refreshToken, socialType, value)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	void 실패_profileImageUrl_은_null_이거나_공백일_수_없다(String value) {
+		assertThatThrownBy(() -> createMember(value, nickname, refreshToken, socialType, socialId)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
 	}
 
 	@Test
-	void 실패_socialId_는_null_일수없다() {
-		// given
-		String socialId = null;
-
-		// when then
-		assertThatThrownBy(() -> createMember("url", "닉네임",
-			1, 1000, 10, "token", Social.GOOGLE, socialId))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("소셜 ID가 Null 일 수 없습니다. [" + socialId + "]");
-	}
-
-	@Test
-	void 실패_socialId_는_공백_일수없다() {
-		// given
-		String socialId = "";
-
-		// when then
-		assertThatThrownBy(() -> createMember("url", "닉네임",
-			1, 1000, 10, "token", Social.GOOGLE, socialId))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("소셜 ID가 공백일 수 없습니다. [" + socialId + "]");
-	}
-
-	@Test
-	void 실패_profileImageUrl_은_null_일수없다() {
-		// given
-		String profileImageUrl = null;
-
-		// when then
-		assertThatThrownBy(() -> createMember(profileImageUrl, "닉네임",
-			1, 1000, 10, "token", Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("프로필 이미지 URL 이 Null 일 수 없습니다. [" + profileImageUrl + "]");
-	}
-
-	@Test
-	void 실패_profileImageUrl_은_공백_일수없다() {
-		// given
-		String profileImageUrl = "";
-
-		// when then
-		assertThatThrownBy(() -> createMember(profileImageUrl, "닉네임",
-			1, 1000, 10, "token", Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("프로필 이미지 URL 이 공백일 수 없습니다. [" + profileImageUrl + "]");
-	}
-
-	@Test
-	void 실패_profileImageUrl_의_길이는2000자보다_클수없다() {
-		// given
-		StringBuilder profileImageUrl = new StringBuilder();
-		while (profileImageUrl.length() <= 2000) {
-			profileImageUrl.append("profile_image_url");
+	void 실패_profileImageUrl_의_길이는_2000자보다_클_수_없다() {
+		StringBuilder url = new StringBuilder();
+		while (url.length() <= 2000) {
+			url.append("profile_image_url");
 		}
 
-		// when then
-		assertThatThrownBy(() -> createMember(profileImageUrl.toString(), "닉네임",
-			1, 1000, 10, "token", Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("프로필 이미지 URL 이 2000자보다 더 길 수 없습니다. [" + profileImageUrl + "]");
+		assertThatThrownBy(
+			() -> createMember(url.toString(), nickname, refreshToken, socialType, socialId)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	void 실패_nickname_은_null_이거나_공백일_수_없다(String value) {
+		assertThatThrownBy(
+			() -> createMember(profileImageUrl, value, refreshToken, socialType, socialId)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
 	}
 
 	@Test
-	void 실패_nickname_이_null_일수없다() {
-		// given
-		String nickname = null;
-
-		// when then
-		assertThatThrownBy(() -> createMember("profile", nickname,
-			1, 1000, 10, "token", Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("닉네임이 Null 일 수 없습니다. [" + nickname + "]");
-	}
-
-	@Test
-	void 실패_nickname_이_공백_일수없다() {
-		// given
-		String nickname = "";
-
-		// when then
-		assertThatThrownBy(() -> createMember("profile", nickname,
-			1, 1000, 10, "token", Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("닉네임이 공백일 수 없습니다. [" + nickname + "]");
-	}
-
-	@Test
-	void 실패_nickname_길이가_24자보다_클수없다() {
-		// given
-		StringBuilder nickname = new StringBuilder();
-		while (nickname.length() <= 24) {
-			nickname.append("name");
+	void 실패_nickname_길이가_24자보다_클_수_없다() {
+		StringBuilder name = new StringBuilder();
+		while (name.length() <= 24) {
+			name.append("name");
 		}
 
-		// when then
-		assertThatThrownBy(() -> createMember("profile", nickname.toString(),
-			1, 1000, 10, "token", Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("닉네임의 길이는 24보다 더 길 수 없습니다. [" + nickname + "]");
+		assertThatThrownBy(() -> createMember(profileImageUrl, name.toString(), refreshToken, socialType,
+			socialId)).isExactlyInstanceOf(IllegalArgumentException.class);
 	}
 
-	@Test
-	void 실패_refreshToken_이_null_일수없다() {
-		// given
-		String refreshToken = null;
-
-		// when then
-		assertThatThrownBy(() -> createMember("profile", "nickname",
-			1, 1000, 10, refreshToken, Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("refresh token 이 Null 일 수 없습니다. [" + refreshToken + "]");
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	void 실패_refreshToken_이_null_이거나_공백일_수_없다(String value) {
+		assertThatThrownBy(
+			() -> createMember(profileImageUrl, nickname, value, socialType, socialId)).isExactlyInstanceOf(
+			IllegalArgumentException.class);
 	}
 
-	@Test
-	void 실패_refreshToken_이_공백_일수없다() {
-		// given
-		String refreshToken = "";
-
-		// when then
-		assertThatThrownBy(() -> createMember("profile", "nickname",
-			1, 1000, 10, refreshToken, Social.GOOGLE, "1234"))
-			.isExactlyInstanceOf(IllegalArgumentException.class)
-			.hasMessage("refresh token 이 공백일 수 없습니다. [" + refreshToken + "]");
-	}
-
-	private Member createMember(String profileImageUrl, String nickname, int ranking,
-		int victoryPoint, int victoryCount, String refreshToken, Social socialType, String socialId) {
+	private Member createMember(String profileImageUrl, String nickname, String refreshToken, Social socialType,
+		String socialId) {
 
 		return Member.builder()
 			.profileImageUrl(profileImageUrl)
@@ -220,5 +146,4 @@ class MemberTest {
 			.socialId(socialId)
 			.build();
 	}
-
 }
