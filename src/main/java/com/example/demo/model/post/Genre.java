5954b95e58315f5a3c7dc65e0297a1bf611993b9
thguss <thgus345@naver.com>
2023-02-21 14:52:01 +0900

    [DEL] 미사용 메소드 삭제

--- src/main/java/com/example/demo/model/post/Genre.java
+++ src/main/java/com/example/demo/model/post/Genre.java
@@ -1,9 +1,5 @@
 package com.example.demo.model.post;
 
-import java.util.Arrays;
-
-import com.google.common.base.Preconditions;
-
 import lombok.Getter;
 
 @Getter
@@ -13,17 +9,6 @@ public enum Genre {
 	final String name;
 
 	Genre(String name) {
-		Preconditions.checkArgument(isPresentGenre(name), "해당 노래 장르가 존재하지 않습니다.", name);
 		this.name = name;
-	}
-
-	public static boolean ofValue(String genreString) {
-		return Arrays.stream(Genre.values())
-			.anyMatch(genre -> genre.name().equals(genreString));
-	}
-
-	private boolean isPresentGenre(String name) {
-		return Arrays.stream(Genre.values())
-			.anyMatch(genre -> genre.getName().equals(name));
 	}
 }
