2e5262a2c5cf9c0eb8883d121a38d2b9a2fb3f19
thguss <thgus345@naver.com>
2023-02-21 14:15:09 +0900

    [FIX] 최소 길이 제한 기준 변경

--- src/main/java/com/example/demo/model/member/Member.java
+++ src/main/java/com/example/demo/model/member/Member.java
@@ -78,7 +78,7 @@ public class Member extends BaseEntity {
 
 		checkArgument(Objects.nonNull(nickname), "닉네임이 Null일 수 없습니다.", nickname);
 		checkArgument(!nickname.isBlank(), "닉네임이 공백일 수 없습니다.", nickname);
-		checkArgument(1 <= nickname.length() && nickname.length() <= 24, "닉네임의 길이는 1보다 짧거나 24보다 더 길 수 없습니다.", nickname);
+		checkArgument(nickname.length() <= 24, "닉네임의 길이는 24보다 더 길 수 없습니다.", nickname);
 
 		checkArgument(Objects.nonNull(refreshToken), "refresh token이 Null일 수 없습니다.", refreshToken);
 		checkArgument(!refreshToken.isBlank(), "refresh token이 공백일 수 없습니다.", refreshToken);
