8cc2eb33c9800f6f63768b977d95e4a5706145c7
thguss <thgus345@naver.com>
2023-02-23 02:42:06 +0900

    [FEAT] 공유 게시글 전체 조회 기능 구현

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -3,15 +3,19 @@ package com.example.demo.controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.common.ApiResponse;
 import com.example.demo.common.ResponseMessage;
 import com.example.demo.dto.post.PostCreateRequestDto;
+import com.example.demo.dto.post.PostsFindResponseDto;
+import com.example.demo.model.post.Genre;
 import com.example.demo.service.PostService;
 
 import lombok.RequiredArgsConstructor;
@@ -35,6 +39,15 @@ public class PostController {
 		ApiResponse apiResponse = ApiResponse.success(ResponseMessage.SUCCESS_CREATE_POST.getMessage());
 
 		return ResponseEntity.created(location).body(apiResponse);
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResponse> findAllPosts(@RequestParam(name = "genre", required = false) Genre genre,
+		@RequestParam(name = "possible", required = false) Boolean possible) {
+		PostsFindResponseDto posts = postService.findAllPosts(genre, possible);
+
+		ApiResponse apiResponse = ApiResponse.success(ResponseMessage.SUCCESS_FIND_ALL_POST.getMessage(), posts);
+		return ResponseEntity.ok(apiResponse);
 	}
 
 }
