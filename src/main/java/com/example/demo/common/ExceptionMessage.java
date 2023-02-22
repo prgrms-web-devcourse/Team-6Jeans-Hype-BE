5d27f6db6658172f345ebe31cc1be05600c6c535
박세준 <tpwns6693@gmail.com>
2023-02-22 14:22:42 +0900

    [FIX] OBJECT_NOT_NULL 의 메시지 변경

--- src/main/java/com/example/demo/common/ExceptionMessage.java
+++ src/main/java/com/example/demo/common/ExceptionMessage.java
@@ -4,7 +4,7 @@ public enum ExceptionMessage {
 	// EntityNotFoundException
 	NOT_FOUND_POST("존재하지 않는 게시글입니다."),
 	NOT_FOUND_MEMBER("존재하지 않는 유저입니다."),
-	OBJECT_NOT_NULL("는 Null 일 수 없습니다.");
+	OBJECT_NOT_NULL("은(는) Null 일 수 없습니다.");
 
 	final String message;
 
