ec36802bb5f2445c7bf0cbb1fc75f1b8cf08af56
thguss <thgus345@naver.com>
2023-02-22 19:49:32 +0900

    [FEAT] 음악 공유 게시글 등록 기능 구현

---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.post.PostCreateRequestDto;
+import com.example.demo.model.member.Member;
+import com.example.demo.model.member.Social;
+import com.example.demo.model.post.Post;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PostService {
+
+	private final PostRepository postRepository;
+	private final MemberRepository memberRepository;
+
+	@Transactional
+	public Long createPost(PostCreateRequestDto postRequestDto) {
+		// 임의 유저 생성: 소셜 로그인 구현 후 삭제
+		Member member = new Member("url", "name", 0, 1,
+			10, 10, "token", Social.GOOGLE, "social");
+		memberRepository.save(member);
+
+		Post post = postRequestDto.toEntity(member);
+		postRepository.save(post);
+
+		return post.getId();
+	}
+}
