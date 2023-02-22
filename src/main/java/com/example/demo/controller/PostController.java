61099a3edd947ca5320d12fd62769f216b7af0b9
thguss <thgus345@naver.com>
2023-02-22 19:48:12 +0900

    [FIX] DTO to Dto 파일명 수정

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.common.ApiResponse;
 import com.example.demo.common.ResponseMessage;
-import com.example.demo.dto.post.PostCreateRequestDTO;
+import com.example.demo.dto.post.PostCreateRequestDto;
 import com.example.demo.service.PostService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,7 +24,7 @@ public class PostController {
 	private final PostService postService;
 
 	@PostMapping
-	public ResponseEntity<ApiResponse> createPost(@RequestBody PostCreateRequestDTO postRequestDto) {
+	public ResponseEntity<ApiResponse> createPost(@RequestBody PostCreateRequestDto postRequestDto) {
 		Long postId = postService.createPost(postRequestDto);
 
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
