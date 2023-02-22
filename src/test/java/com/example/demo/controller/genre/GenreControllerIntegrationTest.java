d649663c771b8b9788e4baafe12fc21f9af7af7a
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-02-23 03:25:25 +0900

    [TEST] 장르 컨트롤러에 대한 통합 테스트 추가. - TestRestTemplate를 이용하여 Rest API에 대한 통합 테스트 작성. - TestRestTemplate에 관한 자료 : https://easybrother0103.tistory.com/64

---
+++ src/test/java/com/example/demo/controller/genre/GenreControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller.genre;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.common.ApiResponse;
+import com.example.demo.dto.genre.GenreAllResponseDto;
+import com.example.demo.model.post.Genre;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GenreControllerIntegrationTest {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	private final GenreController genreController = new GenreController();
+
+	@LocalServerPort
+	private int port;
+
+	@Test
+	public void 성공_모든_장르_정보를_조회할_수_있다() {
+		// given
+		GenreAllResponseDto genreAllResponseDto = GenreAllResponseDto.toEntity(Genre.values());
+		// when
+		ResponseEntity<ApiResponse> response = restTemplate.getForEntity("/genres", ApiResponse.class);
+		// then
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().success()).isTrue();
+		assertThat(response.getBody()).isNotNull();
+	}
+}
