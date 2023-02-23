092eec799fae7b8b98304f3e413f77e279356c3e
park-se-jun <tpwns6693@gmail.com>
2023-02-23 14:19:38 +0900

    [FEAT] cors config 작성
    - 아직 동작 x

---
+++ src/main/java/com/example/demo/config/WebMVcConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMVcConfig implements WebMvcConfigurer {
+	private final long MAX_AGE_SECS = 3600;
+
+	// @Value("${app.cors.allowedOrigins}")
+	private String[] allowedOrigins;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins(allowedOrigins)
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(MAX_AGE_SECS);
+	}
+}
+}
