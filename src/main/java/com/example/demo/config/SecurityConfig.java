ffa05305af7e8d7be05e9aadf7a2ee5f0e86cc84
park-se-jun <tpwns6693@gmail.com>
2023-02-23 14:21:04 +0900

    [FEAT] basic security config 작성
    - 아직 동작 x

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.cors()
+				.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.csrf()
+			.disable()
+			.formLogin()
+			.disable()
+			.httpBasic()
+			.disable()
+			.authorizeRequests()
+			.antMatchers("/",
+				"/error",
+				"/favicon.ico",
+				"/**/*.png",
+				"/**/*.gif",
+				"/**/*.svg",
+				"/**/*.jpg",
+				"/**/*.html",
+				"/**/*.css",
+				"/**/*.js")
+			.permitAll()
+			.antMatchers("/auth/**", "/oauth2/**")
+			.permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.oauth2Login()
+			.authorizationEndpoint()
+			.baseUri("/oauth2/authorization") //로그인페이지를 받기위한 서버의 엔드포인트 설정
+			.authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository())
+			.and()
+			.redirectionEndpoint()
+			.baseUri("/*/oauth2/code/*")
+			.and()
+			.userInfoEndpoint()
+			.userService(customOauth2UserService)
+			.and()
+			.successHandler(oAuth2AuthenticationSuccessHandler(userService));
+		return httpSecurity.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.build();
+	}
+}
