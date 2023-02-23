9b3f2d8e5030b0a0eaef4b0ff45a5aba3cfc41e7
thguss <thgus345@naver.com>
2023-02-23 13:40:09 +0900

    [FIX] 체크스타일 반영

--- src/main/java/com/example/demo/model/member/SocialInfo.java
+++ src/main/java/com/example/demo/model/member/SocialInfo.java
@@ -28,7 +28,6 @@ class SocialInfo {
 
 	SocialInfo(Social socialType, String socialId) {
 		validateSocialInfo(socialType, socialId);
-    
 		this.socialType = socialType;
 		this.socialId = socialId;
 	}
