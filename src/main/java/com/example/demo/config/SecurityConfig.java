be1aae2d69cf4c37e9fb907e9727d6e480d6fdff
park-se-jun <tpwns6693@gmail.com>
2023-02-24 17:00:50 +0900

    [FEAT] 중간 때 참조 했던 코드들 붙여넣기( 이걸로 안할 수도 있음)

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.example.demo.controller.UserService;
+import com.example.demo.security.oauth2.OAuth2AuthenticationSuccessHandler;
+
+@Configuration
 public class SecurityConfig {
+	private final UserService userService;
+
+	public SecurityConfig(UserService userService) {
+		this.userService = userService;
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
@@ -40,18 +50,20 @@ public class SecurityConfig {
 			.and()
 			.oauth2Login()
 			.authorizationEndpoint()
-			.baseUri("/oauth2/authorization") //로그인페이지를 받기위한 서버의 엔드포인트 설정
+			.baseUri("/oauth2/authorize") //로그인페이지를 받기위한 서버의 엔드포인트 설정
 			// .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository())
 			.and()
 			.redirectionEndpoint()
-			.baseUri("/*/oauth2/code/*");
-		// 	.and()
-		// 	.userInfoEndpoint()
-		// 	.userService(customOauth2UserService)
-		// 	.and()
-		// 	.successHandler(oAuth2AuthenticationSuccessHandler(userService));
+			.baseUri("/*/oauth2/code/*")
+			.and()
+			.successHandler(oAuth2AuthenticationSuccessHandler(userService));
 		// return httpSecurity.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 		// 	.build();
 		return httpSecurity.build();
+	}
+
+	@Bean
+	public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler(UserService userService) {
+		return new OAuth2AuthenticationSuccessHandler(userService, tokenProvider);
 	}
 }
