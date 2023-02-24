d1e8884c676bb2694fe90dc50af9c8b254ae35b6
park-se-jun <tpwns6693@gmail.com>
2023-02-25 03:07:17 +0900

    [FEAT] TokenProvider를 우리의 코드에 맞게 수정

--- src/main/java/com/example/demo/security/TokenProvider.java
+++ src/main/java/com/example/demo/security/TokenProvider.java
@@ -1,13 +1,15 @@
 package com.example.demo.security;
 
-import com.example.springsocial.config.AppProperties;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+
+import com.example.demo.config.AppProperties;
 
 @Service
 public class TokenProvider {
@@ -20,14 +22,13 @@ public class TokenProvider {
         this.appProperties = appProperties;
     }
 
-    public String createToken(Authentication authentication) {
-        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+    public String createToken(Long userId ) {
 
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + appProperties.getAuth().getTokenExpirationMsec());
 
         return Jwts.builder()
-                .setSubject(Long.toString(userPrincipal.getId()))
+                .setSubject(Long.toString(userId))
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, appProperties.getAuth().getTokenSecret())
