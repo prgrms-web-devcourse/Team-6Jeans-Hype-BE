54877a9d477af3fe96528eedbce9001ee53118da
park-se-jun <tpwns6693@gmail.com>
2023-02-25 03:14:39 +0900

    [FEAT] SecurityConfig수정 - h2-consle 관련 설정 - authorizationRequestRepository 설정 - SucessHandler 설정 - TokenFilter 설정

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,22 +1,32 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.example.demo.controller.UserService;
+import com.example.demo.security.TokenAuthenticationFilter;
+import com.example.demo.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.example.demo.service.MemberService;
 import com.example.demo.security.oauth2.OAuth2AuthenticationSuccessHandler;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
-	private final UserService userService;
 
-	public SecurityConfig(UserService userService) {
-		this.userService = userService;
-	}
+	@Autowired
+	OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
+	@Autowired
+	HttpCookieOAuth2AuthorizationRequestRepository cookieOAuth2AuthorizationRequestRepository;
+
+	@Autowired
+	TokenAuthenticationFilter tokenAuthenticationFilter;
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
@@ -24,6 +34,9 @@ public class SecurityConfig {
 				.and()
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.headers()
+			.frameOptions().disable()
 			.and()
 			.csrf()
 			.disable()
@@ -41,7 +54,8 @@ public class SecurityConfig {
 				"/**/*.jpg",
 				"/**/*.html",
 				"/**/*.css",
-				"/**/*.js")
+				"/**/*.js",
+				"/h2-console/**")
 			.permitAll()
 			.antMatchers("/auth/**", "/oauth2/**")
 			.permitAll()
@@ -51,19 +65,14 @@ public class SecurityConfig {
 			.oauth2Login()
 			.authorizationEndpoint()
 			.baseUri("/oauth2/authorize") //로그인페이지를 받기위한 서버의 엔드포인트 설정
-			// .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository())
+			.authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository)
 			.and()
 			.redirectionEndpoint()
-			.baseUri("/*/oauth2/code/*")
+				.baseUri("/*/oauth2/code/*")
 			.and()
-			.successHandler(oAuth2AuthenticationSuccessHandler(userService));
-		// return httpSecurity.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-		// 	.build();
-		return httpSecurity.build();
+			.successHandler(oAuth2AuthenticationSuccessHandler);
+		return httpSecurity.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
 	}
 
-	@Bean
-	public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler(UserService userService) {
-		return new OAuth2AuthenticationSuccessHandler(userService, tokenProvider);
-	}
 }
