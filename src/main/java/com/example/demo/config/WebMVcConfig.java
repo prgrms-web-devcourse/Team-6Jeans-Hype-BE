f4cd00df246d1680b726193b804d1f5df938f010
park-se-jun <tpwns6693@gmail.com>
2023-02-25 03:34:25 +0900

    [FEAT] CORS 설정 적용되도록 변경 - @Configuration 어노테이셔 추가 - allowedOrigins를 @Value로 꽂아줌

--- src/main/java/com/example/demo/config/WebMVcConfig.java
+++ src/main/java/com/example/demo/config/WebMVcConfig.java
@@ -1,12 +1,15 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMVcConfig implements WebMvcConfigurer {
 	private final long MAX_AGE_SECS = 3600;
 
-	// @Value("${app.cors.allowedOrigins}")
+	@Value("${app.cors.allowedOrigins}")
 	private String[] allowedOrigins;
 
 	@Override
