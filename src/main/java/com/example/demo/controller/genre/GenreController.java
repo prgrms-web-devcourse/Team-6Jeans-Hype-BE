eccd5fe726e33c0d6fd2ab2f2b96b19214426c97
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-02-24 10:22:01 +0900

    [FIX] 코드 리뷰 반영 및 메소드명 변경 - dto 이름 수정. - 테스트 메소드 이름 수정. - dto 메소드명 수정. - url path 수정.

--- src/main/java/com/example/demo/controller/genre/GenreController.java
+++ src/main/java/com/example/demo/controller/genre/GenreController.java
@@ -2,18 +2,23 @@ package com.example.demo.controller.genre;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.common.ApiResponse;
 import com.example.demo.dto.genre.GenreAllResponseDto;
 import com.example.demo.model.post.Genre;
 
-@RestController(value = "/api/v1")
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/genres")
 public class GenreController {
 
-	@GetMapping("/genres")
+	@GetMapping
 	public ResponseEntity<ApiResponse> getAllGenres() {
-		GenreAllResponseDto genreAllResponseDto = GenreAllResponseDto.toEntity(Genre.values());
+		GenreAllResponseDto genreAllResponseDto = GenreAllResponseDto.of(Genre.values());
 		return ResponseEntity.ok(
 			ApiResponse.success(
 				"장르 전체 조회 성공",
