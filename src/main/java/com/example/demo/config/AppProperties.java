bd3d0c7adb94b72f646c9230fd255ced77cdf7d4
park-se-jun <tpwns6693@gmail.com>
2023-02-25 02:47:30 +0900

    [CHORE] OAuth2 login 시 필요한 application.yml 수정 및 그에 해당하는 properties 생성 - client-id, client-secret 같이 필요한 정보들 은닉화 및 추가 - 그외 사용하는 정보 application에 추가 - 추가한 정보 properties로 생성

--- src/main/java/com/example/demo/config/AppProperties.java
+++ src/main/java/com/example/demo/config/AppProperties.java
@@ -1,10 +1,12 @@
 package com.example.demo.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Configuration
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
 	private final Auth auth = new Auth();
