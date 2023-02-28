6e547d3172ace379f6e4571f7c78888431dbb765
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-02-28 19:29:32 +0900

    [FIX] 배틀 엔티티 장르,완료 여부 추가 (#104)
    * [CHORE] Battle 테이블 관련 flyway migration ddl 추가.
    - Battle의 종료 유무를 위한 is_ended 추가
    - Battle의 장르를 위한 genre 추가.
    
    * [ADD] Battle 엔티티 필드 추가.
    - 대결 종류유무를 위한 isEnded 추가.
    - 대결의 장르를 위한 genre 추가.
    
    * [CHORE] ci 통과를 위한 logback.xml과 의존성 추가.
    
    * [TEST] 추가된 컬럼에 따른 테스트 수정 및 추가.
    - genre가 null인 경우에 대한 테스트 추가.
    
    * [CHORE] flyway migration을 위한 ddl 추가.
    
    * [FIX] 대결 엔티티의 상태를 표시하기 위한 필드를 boolean 타입에서 enum을 이용하게 수정.
    - BattleStatus 추가.
    - BattleTest에 enum에 대한 테스트 추가.
    - Battle 필드 수정.

--- src/main/java/com/example/demo/model/battle/Battle.java
+++ src/main/java/com/example/demo/model/battle/Battle.java
@@ -9,13 +9,17 @@ import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.validation.constraints.NotNull;
 
 import com.example.demo.common.ExceptionMessage;
 import com.example.demo.model.BaseEntity;
+import com.example.demo.model.post.Genre;
 import com.example.demo.model.post.Post;
 
 import lombok.AccessLevel;
@@ -32,6 +36,15 @@ public class Battle extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotNull
+	@Enumerated(value = EnumType.STRING)
+	private Genre genre;
+
+	@NotNull
+	@Enumerated(value = EnumType.STRING)
+	@Column(name = "status")
+	private BattleStatus status;
+
 	@Embedded
 	@AttributeOverride(name = "voteCount", column = @Column(name = "challenged_vote_count"))
 	@AssociationOverride(name = "post", joinColumns = @JoinColumn(name = "challenged_post_id"))
@@ -43,11 +56,15 @@ public class Battle extends BaseEntity {
 	private BattleInfo challengingPost;
 
 	@Builder
-	public Battle(Post challengedPost, Post challengingPost) {
+	public Battle(Genre genre, BattleStatus status, Post challengedPost, Post challengingPost) {
 		String errorMessageForNullPost = String.format("POST %s", ExceptionMessage.OBJECT_NOT_NULL.getMessage());
+		checkArgument(Objects.nonNull(genre), errorMessageForNullPost);
+		checkArgument(Objects.nonNull(status), errorMessageForNullPost);
 		checkArgument(Objects.nonNull(challengedPost), errorMessageForNullPost);
 		checkArgument(Objects.nonNull(challengingPost), errorMessageForNullPost);
 
+		this.genre = genre;
+		this.status = status;
 		this.challengedPost = new BattleInfo(challengedPost);
 		this.challengingPost = new BattleInfo(challengingPost);
 	}
