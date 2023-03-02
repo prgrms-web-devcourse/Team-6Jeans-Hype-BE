5e7e662298de31c1467dff00d439a4dbc7a06274
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-02 17:19:55 +0900

    [FEAT] 대결 투표 기능 추가. - voteService 추가. - voteRepository 추가.

---
+++ src/main/java/com/example/demo/service/VoteService.java
@@ -0,0 +1,53 @@
+package com.example.demo.service;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.common.ExceptionMessage;
+import com.example.demo.dto.vote.VoteResultResponseDto;
+import com.example.demo.model.battle.Battle;
+import com.example.demo.model.battle.BattleVotedResult;
+import com.example.demo.model.battle.Vote;
+import com.example.demo.model.member.Member;
+import com.example.demo.model.post.Post;
+import com.example.demo.repository.BattleRepository;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.repository.VoteRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class VoteService {
+
+	private final BattleRepository battleRepository;
+	private final PostRepository postRepository;
+	private final VoteRepository voteRepository;
+
+	@Transactional
+	public VoteResultResponseDto voteBattle(Member member, Long battleId, Long postId) {
+		Battle battle = battleRepository.findByIdPessimisticLock(battleId)
+			.orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.NOT_FOUND_BATTLE.getMessage()));
+		Post post = postRepository.findById(postId)
+			.orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.NOT_FOUND_POST.getMessage()));
+
+		BattleVotedResult battleVotedResult = battle.vote(post);
+		saveVote(battle, post, member);
+
+		return new VoteResultResponseDto(
+			post.getMusic().getTitle(),
+			post.getMusic().getAlbumCoverUrl(),
+			battleVotedResult.selectedPostVoteCnt(),
+			battleVotedResult.oppositePostVoteCnt()
+		);
+	}
+
+	private void saveVote(Battle battle, Post post, Member member) {
+		Vote vote = Vote.createVote(battle, post, member);
+		voteRepository.save(vote);
+	}
+}
