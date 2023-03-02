4189e0b78b81203832bcb2cacd51c403933c09ce
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-03 04:11:45 +0900

    [CHORE] ResponseMessage 추가.

--- src/main/java/com/example/demo/common/ResponseMessage.java
+++ src/main/java/com/example/demo/common/ResponseMessage.java
@@ -10,7 +10,8 @@ public enum ResponseMessage {
 	SUCCESS_FIND_POST("음악 공유 게시글 상세 조회 성공"),
 	SUCCESS_FIND_ALL_CANDIDATE_POST("대결곡 후보 게시글 리스트 조회 성공"),
 	SUCCESS_FIND_ALL_BATTLE_DETAILS("대결 상세 정보 리스트 조회 성공"),
-	SUCCESS_FIND_BATTLE_BY_MEMBER("유저 참여 대결 정보 리스트 조회 성공");
+	SUCCESS_FIND_BATTLE_BY_MEMBER("유저 참여 대결 정보 리스트 조회 성공"),
+	SUCCESS_VOTE("대결 투표 성공");
 
 	final String message;
 
