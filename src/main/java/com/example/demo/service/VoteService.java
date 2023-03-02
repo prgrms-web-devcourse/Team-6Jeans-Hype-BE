adec1fad6936694c171da86d47a11e353b4e0385
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-03 00:04:19 +0900

    [DEL] @Slf4j 어노테이션 삭제

--- src/main/java/com/example/demo/service/VoteService.java
+++ src/main/java/com/example/demo/service/VoteService.java
@@ -17,9 +17,7 @@ import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.VoteRepository;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class VoteService {
