3573e34ad9d2c510afef521832afc38489a2b630
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-03 16:45:30 +0900

    [FEAT] 내가 투표한 대결인지를 확인하기 위한 vote 조회 쿼리 메소드 추가.

--- src/main/java/com/example/demo/repository/VoteRepository.java
+++ src/main/java/com/example/demo/repository/VoteRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.model.battle.Battle;
 import com.example.demo.model.battle.Vote;
+import com.example.demo.model.member.Member;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+	Optional<Vote> findByBattleAndVoter(Battle battle, Member voter);
 }
