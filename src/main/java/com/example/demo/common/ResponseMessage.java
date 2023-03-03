32eb32daa3a375d5f708aee5e649218c048604b2
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-03 17:51:52 +0900

    [FIX] MemberController에서 마이페이지와 유저페이지를 나누는 분기가 들어가게 수정. - 응답 성공 메시지 추가.

--- src/main/java/com/example/demo/common/ResponseMessage.java
+++ src/main/java/com/example/demo/common/ResponseMessage.java
@@ -4,13 +4,17 @@ import lombok.Getter;
 
 @Getter
 public enum ResponseMessage {
+	// Member
+	SUCCESS_FIND_BATTLE_BY_MEMBER("유저 참여 대결 정보 리스트 조회 성공"),
+	SUCCESS_MY_PAGE_PROFILE("마이페이지 상세정보 조회 성공"),
+	SUCCESS_USER_PAGE_PROFILE("유저페이지 상세정보 조회 성공"),
 	// Post
 	SUCCESS_CREATE_POST("음악 공유 게시글 등록 성공"),
 	SUCCESS_FIND_ALL_POST("음악 공유 게시글 리스트 조회 성공"),
 	SUCCESS_FIND_POST("음악 공유 게시글 상세 조회 성공"),
+	// Battle
 	SUCCESS_FIND_ALL_CANDIDATE_POST("대결곡 후보 게시글 리스트 조회 성공"),
 	SUCCESS_FIND_ALL_BATTLE_DETAILS("대결 상세 정보 리스트 조회 성공"),
-	SUCCESS_FIND_BATTLE_BY_MEMBER("유저 참여 대결 정보 리스트 조회 성공"),
 	SUCCESS_VOTE("대결 투표 성공");
 
 	final String message;
