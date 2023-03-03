100cb1d88a93d958f76e5d63cc5d9ab94406474b
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-03 18:48:50 +0900

    [FIX] 참여한 대결 리스트 응답 데이터 수정

--- src/main/java/com/example/demo/dto/member/MemberBattlePostVO.java
+++ src/main/java/com/example/demo/dto/member/MemberBattlePostVO.java
@@ -8,12 +8,14 @@ import reactor.util.annotation.NonNull;
 @Builder
 public record MemberBattlePostVO(
 	@NonNull String title,
+	@NonNull String singer,
 	@NonNull String albumUrl,
 	@NonNull String nickname) {
 
 	public static MemberBattlePostVO of(Post post) {
 		return MemberBattlePostVO.builder()
 			.title(post.getMusic().getTitle())
+			.singer(post.getMusic().getSinger())
 			.albumUrl(post.getMusic().getAlbumCoverUrl())
 			.nickname(post.getMember().getNickname())
 			.build();
