400b6ec0a9c96d5104e6c557fff3574908da93f6
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-03 16:57:43 +0900

    [CHORE] 예외 메시지 추가 및 핸들러 추가.

--- src/main/java/com/example/demo/common/ExceptionMessage.java
+++ src/main/java/com/example/demo/common/ExceptionMessage.java
@@ -11,7 +11,7 @@ public enum ExceptionMessage {
 
 	// IllegalArgumentException
 	DUPLICATED_USER_MUSIC_URL("이미 공유한 곡입니다."),
-	DUPLICATED_USER_VOTE("해당 유저가 이미 투표한 대결입니다."),
+	DUPLICATED_USER_VOTE("이미 투표한 대결입니다."),
 	SERVER_ERROR("서버 내부에 오류가 발생했습니다."),
 	NOT_FOUND_BATTLE("존재하지 않는 대결입니다."),
 	POST_NOT_CONTAIN_BATTLE("투표한 게시글이 대결에 존재하지 않습니다.");
