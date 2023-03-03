5399355163523ab219b520748cf2527d9866ce10
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-03 15:13:32 +0900

    [ADD] 특정 유저의 투표 리스트 조회 기능 추가.

--- src/main/java/com/example/demo/repository/VoteRepository.java
+++ src/main/java/com/example/demo/repository/VoteRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.battle.Vote;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+	List<Vote> findAllByVoterId(Long voterId);
 }
