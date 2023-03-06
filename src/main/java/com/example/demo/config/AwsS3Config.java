ff4c286dfc99eb0b4bffb86ea70c168edfb06cc7
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-06 18:35:45 +0900

    [FEAT] s3에 접근하기 위하여 sdk 서비스를 이용한 빈 객체 생성 configuration 추가.

---
+++ src/main/java/com/example/demo/config/AwsS3Config.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AwsS3Config {
+
+	@Value("${cloud.aws.credentials.accessKey}")
+	private String accessKey;
+
+	@Value("${cloud.aws.credentials.secretKey}")
+	private String secretKey;
+
+	@Bean
+	public AmazonS3 amazonS3() {
+		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+		return AmazonS3ClientBuilder.standard()
+			.withCredentials(new AWSStaticCredentialsProvider(credentials))
+			.withRegion(Regions.AP_NORTHEAST_2)
+			.build();
+	}
+}
