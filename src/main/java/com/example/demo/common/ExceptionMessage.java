b8178610907a365ef062da82879f1690ce27bc5a
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-06 20:47:48 +0900

    [CHORE] 실수로 옮긴 ExceptionMessage 다시 이동.

--- src/main/java/com/example/demo/common/ExceptionMessage.java
+++ src/main/java/com/example/demo/common/ExceptionMessage.java
@@ -11,6 +11,7 @@ public enum ExceptionMessage {
 	NOT_FOUND_MEMBER("존재하지 않는 유저입니다."),
 	OBJECT_NOT_NULL("은(는) Null 일 수 없습니다."),
 	CANNOT_MAKE_BATTLE_WRONG_POST_ID("대결을 생성할 수 있는 알맞는 PostId를 찾을 수 없습니다."),
+
 	// IllegalArgumentException
 	DUPLICATED_USER_MUSIC_URL("이미 공유한 곡입니다."),
 	DUPLICATED_USER_VOTE("이미 투표한 대결입니다."),
@@ -21,10 +22,11 @@ public enum ExceptionMessage {
 	CANNOT_MAKE_BATTLE_OWN_CHALLENED_POST("자신의 post에 배틀을 신청할 수 없습니다."),
 	CANNOT_MAKE_BATTLE_DIFFERENT_GENRE("두 post의 음악 장르가 다릅니다."),
 	CANNOT_MAKE_BATTLE_SAME_MUSIC("두 Post가 같은 음악에 대한 Post입니다."),
-	CANNOT_MAKE_BATTLE_NOT_ENOUGH_CHALLENGE_TICKET("사용자의 대결권이 부족합니다."),
 	NOT_ALLOWED_FILE_FORMAT("허용되지 않는 파일 형식입니다."),
+
 	// IllegalStateException
 	FAIL_UPLOAD_FILE_S3("저장소에 파일 업로드가 실패했습니다."),
+	CANNOT_MAKE_BATTLE_NOT_ENOUGH_CHALLENGE_TICKET("사용자의 대결권이 부족합니다."),
 
 	// MethodArgumentNotValidException
 	CANNOT_ALL_REQUEST_FIELDS_NULL("적어도 하나의 필드(닉네임 또는 프로필 이미지 URL)를 제공해야합니다."),
