41ce4d4d38ebc4723756c191e868f40b0a2b1c6a
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-03-10 15:36:02 +0900

    [CHORE] 충돌해결

--- src/main/java/com/example/demo/common/ResponseMessage.java
+++ src/main/java/com/example/demo/common/ResponseMessage.java
@@ -24,9 +24,7 @@ public enum ResponseMessage {
 	SUCCESS_CREATE_BATTLE("대결 등록 성공"),
 	SUCCESS_FIND_BATTLES("대결 리스트 조회 성공"),
 	SUCCESS_FIND_BATTLE_DETAIL_BY_ID("배틀 상세 조회 성공"),
-  SUCCESS_RANDOM_BATTLE("랜덤한 대결 상세 조회 성공"),
-	SUCCESS_LIKE_POST("추천글 좋아요 성공"),
-	SUCCESS_UNLIKE_POST("추천글 좋아요 해제 성공"),
+	SUCCESS_RANDOM_BATTLE("랜덤한 대결 상세 조회 성공"),
 	SUCCESS_GET_IS_LIKE("좋아요 여부 판단 성공");
 
 	final String message;
